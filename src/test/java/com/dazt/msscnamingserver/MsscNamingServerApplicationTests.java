38091663c1f4022bc24c80c5ca0d0c0d8adc23f7
Diego <diego.zavala.t@gmail.com>
2021-04-26 12:51:40 -0500

    Initial commit. Settup Eureka as a Naming Server.

---
+++ src/test/java/com/dazt/msscnamingserver/MsscNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazt.msscnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
